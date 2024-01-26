01e86385bbf8491b4f012f0189e9caa14ca53c55
Vito Senic <67893442+pegi4@users.noreply.github.com>
2024-01-26 10:04:08 +0100

    Klikne, če se da upgradat

--- tw.js
+++ tw.js
@@ -7,8 +7,17 @@ function avtomatskaNadgradnja() {
 
     vrsticeZgradb.forEach(function(vrstica) {
 
+        //samo zgradbe, ki so v array-u upgrade_buildings
         if(upgrade_building.includes(vrstica.id)) {
             //console.log(vrstica.id);
+            var buildOptions = vrstica.querySelector('.build_options');
+
+            var povezavaZaKlik = buildOptions.querySelectorAll('[id^="main_buildlink_"]')
+
+            console.log(povezavaZaKlik[1])
+
+            povezavaZaKlik[1].click();
+
         }
 
     });
