6d6fd077612cbae26e35cc1d59fc93a92841e8b5
Vito Senic <67893442+pegi4@users.noreply.github.com>
2024-01-27 14:57:43 +0100

    small struct update

--- gm_script.js
+++ gm_script.js
@@ -4,12 +4,15 @@
 // @version      1.0
 // @description  Avtomatično nadgradi zgradbe v igri
 // @author       Pegi4
-// @match        https://en136.tribalwars.net/game.php?village=50038&screen=main
+// @match        https://en137.tribalwars.net/game.php?village=73339&screen=main
 // @grant        none
 // ==/UserScript==
 
 (function() {
     'use strict';
+
+    //zgradbe za upgrade
+    let upgrade_buildings = ["wood", "stone", "iron"];
 
     function randomIntFromInterval(min, max) { // min and max included 
         return Math.floor(Math.random() * (max - min + 1) + min)
@@ -45,28 +48,22 @@
     }    
 
     function buildqueue() {
-    
         let buildqueue = document.getElementById('buildqueue');
-
         // if nothing is in queue
         if (!buildqueue) {
             return 0;
         } else {
             let buildOrder = buildqueue.querySelectorAll('[class*="buildorder_"]');
-
             return buildOrder.length;
         }
-
     }
 
-    function nadgradiZgradbo() {
-        //zgradbe za upgrade
-        let upgrade_buildings = ["wood", "stone", "iron"];
+    function upGradeBuilding() {
         // Poišči vse vrstice zgradb
         let vrsticeZgradb = document.querySelectorAll('.build_options');
         //zgradba najnižje stopnje za upgradat
         let stavba_min = null;
-        let najnizji_lvl = 100;
+        let najnizji_lvl = Infinity;
 
         vrsticeZgradb.forEach(function(vrstica) {
             let povezava = vrstica.querySelectorAll('a[data-building]');
@@ -80,32 +77,37 @@
                 let building = povezava[1];
                 if (building.style.display !== "none" && upgrade_buildings.includes(building.dataset.building)) {
                     let building_lvl = parseInt(building.dataset.levelNext);
-
                     if (building_lvl < najnizji_lvl) {
                         najnizji_lvl = building_lvl;
                         stavba_min = building;
                     }
-                }
+                } 
             }
         });
-        if (stavba_min) {
-            console.log('Zgradi: ' + stavba_min.dataset.building + ' na stopnjo: ' + stavba_min.dataset.levelNext);
-            //stavba_min.click();
+        return stavba_min;
+    }
+
+    function nadgradiZgradbo() {
+        let upGB = upGradeBuilding();
+        if (upGB) {
+            console.log('Zgradi: ' + upGB.dataset.building + ' na stopnjo: ' + upGB.dataset.levelNext);
+            //upGB.click();
         } else {
-            console.log('Dobene zgradbe ni mogoce nadgraditi.');
+            console.log('Ni dovolj surovin za nadgradnjo.');
         }
     }
 
     function avtomatskaNadgradnja() {
+        console.log("Start");
         let upgrade_time = randomIntFromInterval(3000,5000);
         let reload_time = randomIntFromInterval(upgrade_time, upgrade_time+2000)
         console.log("Upgrade time: " + upgrade_time);
-        console.log("Reload time: " + reload_time);
-        if (buildqueue() < 2) {
-            console.log("Start");
+        
+        if (buildqueue() < 5) {
             setTimeout(()=>{
                 nadgradiZgradbo();
             },upgrade_time);
+            console.log("Reload time in: " + reload_time);
             setTimeout(()=>{
                 location.reload();
             },reload_time);
